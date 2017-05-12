a2c67cd504a33f334316fd9c341c1f115c328066
VICTOR AGUILUZ <victa727@yahoo.com>
2017-05-11 19:55:35 -0700

    api key

---
+++ app/scripts/services/current.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name weatherTownApp.current
+ * @description
+ * # current
+ * Factory in the weatherTownApp.
+ */
+angular.module('weatherTownApp')
+  .factory('current', function ($resource) {
+    // Service logic
+    // ...
+
+
+
+    // Public API here
+    return $resource('http://api.openweathermap.org/data/2.5/weather?q=:location&units=imperial&APPID=f871cb1a287c72d24f93963850b0e09e', {}, {
+      query: {
+        method:'GET',
+        params:{
+          location: 'Seattle,us'
+        },
+        isArray:false
+      }
+    })
+  });
