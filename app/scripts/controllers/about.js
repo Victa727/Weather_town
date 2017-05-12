cd4e700052069ea6bc2c69a59d9026b6d079e284
VICTOR AGUILUZ <vicelsa@alices-6s.seattleu.edu>
2017-05-11 18:53:04 -0700

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name weatherTownApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the weatherTownApp
+ */
+angular.module('weatherTownApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
