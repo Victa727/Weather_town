7f4bcd8f75453b28ae2155abc290bee858244535
VICTOR AGUILUZ <victa727@yahoo.com>
2017-05-14 21:37:20 -0700

    Gruntbuildcontrol

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,6 +17,7 @@ module.exports = function (grunt) {
     useminPrepare: 'grunt-usemin',
     ngtemplates: 'grunt-angular-templates',
     cdnify: 'grunt-google-cdn'
+    buildcontrol: 'grunt-build-control'
   });
 
   // Configurable paths for the application
