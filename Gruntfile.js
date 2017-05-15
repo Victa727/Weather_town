08e14699578c72217589d2e65b5207d70ba632b7
VICTOR AGUILUZ <victa727@yahoo.com>
2017-05-14 21:51:57 -0700

    fix buildcontrol

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,6 +32,23 @@ module.exports = function (grunt) {
     // Project settings
     yeoman: appConfig,
 
+
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com:Victa727/Weather_town.git',
+          branch: 'gh-pages'
+        }
+      }
+    },
+
+
     // Watches files for changes and runs tasks based on the changed files
     watch: {
       bower: {
