460bdeecb953b799a3cd507e6339579da5f7cbf2
VICTOR AGUILUZ <victa727@yahoo.com>
2017-05-14 21:22:25 -0700

    edits

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,23 +8,14 @@
  * Controller of the weatherTownApp
  */
 angular.module('weatherTownApp')
+ .controller('MainCtrl', function ($scope, citysearch, $localStorage) {
+   $scope.citiesFound = citysearch.find();
+   $scope.storage = $localStorage;
 
-
-.controller('MainCtrl', function ($scope, citysearch, $localStorage) {
-    $scope.citiesFound = citysearch.find();
-    $scope.storage = $localStorage;
-
-    $scope.findCities = function(){
-        $scope.citiesFound = citysearch.find({
-            query: $scope.location
-        });
-        $scope.searchQuery = $scope.location;
-    };
-
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  
-  });
+   $scope.findCities = function(){
+       $scope.citiesFound = citysearch.find({
+           query: $scope.location
+       });
+       $scope.searchQuery = $scope.location;
+   };
+ });
